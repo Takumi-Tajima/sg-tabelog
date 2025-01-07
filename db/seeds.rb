d51134b6cf6c3e58830bf0af67c1e04c218dae09
taji <taji@sonicgarden.jp>
2025-01-08 08:22:32 +0900

    seedsにテストデータ加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+client = GooglePlaces::Client.new(Rails.application.credentials.google.api_key)
+
+spots = client.spots(35.5942061, 139.6735866, radius: 500, types: 'restaurant', language: 'ja')
+
+spots.each do |spot|
+  next unless spot
+
+  Shop.create!(
+    name: spot.name,
+    place_id: spot.place_id
+  )
+end
