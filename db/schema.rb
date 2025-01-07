06553e1d43212f1e58aed8b3dc1e8ea98d54ccc5
taji <taji@sonicgarden.jp>
2025-01-07 17:55:31 +0900

    place_idはを文字列に変更

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_07_084506) do
 
   create_table "shops", force: :cascade do |t|
     t.string "name", null: false
-    t.integer "place_id", null: false
+    t.string "place_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
