b94bfcfe100c340523d10f97c9e48c1226472f3b
taji <taji@sonicgarden.jp>
2025-01-08 08:28:27 +0900

    カラム名変更

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_07_101831) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "e_payment", default: false, null: false
-    t.boolean "card", default: false, null: false
+    t.boolean "card_payment", default: false, null: false
     t.index ["place_id"], name: "index_shops_on_place_id", unique: true
   end
 end
